a8f1935255b1e8c8573eb72d1af5824f59d736b9
FloFranz <florian.franz86@gmx.de>
2023-06-22 14:48:11 +0200

    add blank script lm_timber_vol.R

---
+++ scripts/lm_timber_vol.R
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------
+# Name:         lm_timber_vol.R
+# Description:  script creates a linear model to predict 
+#               timber volume based on remote sensing data and
+#               terrestrial sample plot measurements.
+# Author:       Florian Franz
+# Contact:      florian.franz@nw-fva.de
+#-------------------------------------------------------------
+
+
+
+# source setup script
+source('src/setup.R', local = TRUE)
