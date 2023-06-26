82f81b903111eabff9473de7b35d227de498917b
FloFranz <florian.franz86@gmx.de>
2023-06-26 10:57:11 +0200

    update function format_data.R

--- src/format_data.R
+++ src/format_data.R
@@ -1,8 +1,21 @@
-# function to perform data formatting steps
+#' Format Data Frame by Lowercasing Column Names and Extracting Substrings
+#'
+#' This function takes a data frame as input and performs data formatting steps on it.
+#' It converts column names to lowercase and extracts substrings from the column names
+#' based on the position of the last underscore ("_") character. The extracted substrings
+#' are then set as the new column names.
+#'
+#' @param df A data frame to be formatted.
+#'
+#' @return The formatted data frame with modified column names.
+#'
+#' @examples
+#' df_formatted <- format_data(df)
+#'
 
 format_data <- function(df) {
   
-  # column names to lower case
+  # column names to lowercase
   names(df) <- tolower(names(df))
   
   dat <- data.frame(name = names(df), pos_sep = NA)
