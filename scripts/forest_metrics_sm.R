1fdcaa51634db635b3abadb8a2ec9fcb32be1195
FloFranz <florian.franz86@gmx.de>
2023-08-25 11:53:04 +0200

    add NDVI as new metric to forest_metrics_sm.R

--- scripts/forest_metrics_sm.R
+++ scripts/forest_metrics_sm.R
@@ -2,9 +2,10 @@
 # Name:         forest_metrics_sm.R
 # Description:  Script calculates several metrics in terrestrial
 #               sample plot points based on digital surface models (DSM)
-#               or normalized digital surface models (nDSM), respectively.
+#               or normalized digital surface models (nDSM) at 1 m resolution
+#               and a digital orthophoto (DOP) at 20 cm resolution.
 #               DSM and nDSM were previously derived from image-based point clouds.
-#               This script is designed to test metrics for one single raster.
+#               This script is designed to test metrics for one single DSM/nDSM tile.
 # Author:       Florian Franz
 # Contact:      florian.franz@nw-fva.de
 #----------------------------------------------------------------------------------
@@ -23,6 +24,9 @@ source('src/setup.R', local = TRUE)
 dsm_path <- paste0(raw_data_dir, 'DSMs/')
 ndsm_path <- paste0(raw_data_dir, 'nDSMs/')
 
+# input path to DOP raster file
+dop_path <- paste0(raw_data_dir, 'DOP/')
+
 
 
 # 02 - data reading
@@ -35,6 +39,10 @@ ndsm
 
 # quick plot
 terra::plot(ndsm)
+
+# read DOP
+dop_solling <- terra::rast(paste0(dop_path, 'dop_solling.tif'))
+dop_solling
 
 # read BI data preprocessed in script vol_sample_plots.R
 # contains timber volume per sample points
@@ -77,23 +85,20 @@ bi_plots_cropped_buf <- sf::st_buffer(bi_plots_cropped, dist = 13)
 terra::plot(ndsm)
 plot(bi_plots_cropped_buf$geom, pch = 16, add = T)
 
-# extract the values from the raster (height values of the nDSM)
+# extract the values from the rasters 
+# (height values of the nDSM and spectral values of the DOP)
 # that are within the buffered points (now circles --> sample plots)
-extracted_val <- terra::extract(ndsm, bi_plots_cropped_buf, raw = T)
+extracted_val_ndsm <- terra::extract(ndsm, bi_plots_cropped_buf)
+extracted_val_dop <- terra::extract(dop_solling, bi_plots_cropped_buf)
 
-# convert the resulting matrix to a data frame
-extracted_val_df <- as.data.frame(extracted_val)
-head(extracted_val_df)
-
-# split the data frame into several data frames
-# corresponding to the number of sample plots within the nDSM
-extracted_val_df_list <- split(extracted_val_df, extracted_val_df$ID)
+# split both resulting data frames into several data frames
+# corresponding to the number of sample plots
+extracted_val_ndsm_df_list <- split(extracted_val_ndsm, extracted_val_ndsm$ID)
+extracted_val_dop_df_list <- split(extracted_val_dop, extracted_val_dop$ID)
 
 # the resulting data frames do not all have the same length
 # --> empty rows must be filled with NA
 # function to pad data frames with NA rows
-max_rows <- max(sapply(extracted_val_df_list, nrow))
-
 pad_with_na <- function(df, max_rows) {
   
   if (nrow(df) < max_rows) {
@@ -112,16 +117,37 @@ pad_with_na <- function(df, max_rows) {
   
 }
 
-# apply padding to each data frame in the data frame list
-extracted_val_df_list <- lapply(extracted_val_df_list, pad_with_na, 
-                                max_rows = max_rows)
+# apply padding to each data frame in the data frame lists
+max_rows_ndsm <- max(sapply(extracted_val_ndsm_df_list, nrow))
+max_rows_dop <- max(sapply(extracted_val_dop_df_list, nrow))
+
+extracted_val_ndsm_df_list <- lapply(extracted_val_ndsm_df_list, 
+                                     pad_with_na, 
+                                     max_rows = max_rows_ndsm)
+
+extracted_val_dop_df_list <- lapply(extracted_val_dop_df_list, 
+                                    pad_with_na,
+                                    max_rows = max_rows_dop)
 
 # bind the data frames by columns
-extracted_val_df <- dplyr::bind_cols(extracted_val_df_list)
-head(extracted_val_df)
+extracted_val_ndsm <- dplyr::bind_cols(extracted_val_ndsm_df_list)
+head(extracted_val_ndsm)
+
+extracted_val_dop <- dplyr::bind_cols(extracted_val_dop_df_list)
+head(extracted_val_dop)
+
+# remove data frame lists
+rm(extracted_val_ndsm_df_list, extracted_val_dop_df_list)
+
+# reduce data frames so for each plot 
+# only the extracted values are shown
+# this step is done separately for the nDSM and the DOP
+
+# nDSM
+#-----
 
 # get the number of ID and value column pairs
-num_pairs <- ncol(extracted_val_df) / 2
+num_pairs <- ncol(extracted_val_ndsm) / 2
 
 # vector to store the indices of the ID columns to be removed
 id_columns_to_remove <- c()
@@ -139,7 +165,7 @@ for (i in 1:num_pairs) {
   plot_name <- plot_names[i]
   
   # assign the plot names as the column names
-  colnames(extracted_val_df)[value_column] <- plot_name
+  colnames(extracted_val_ndsm)[value_column] <- plot_name
   
   # store the index of the ID column to be removed
   id_columns_to_remove <- c(id_columns_to_remove, id_column)
@@ -147,9 +173,47 @@ for (i in 1:num_pairs) {
 }
 
 # remove the ID columns
-extracted_val_df <- extracted_val_df[, -id_columns_to_remove]
+extracted_val_ndsm <- extracted_val_ndsm[, -id_columns_to_remove]
+head(extracted_val_ndsm)
 
-head(extracted_val_df)
+# DOP
+#----
+
+# number of plot names
+num_plot_names <- 10
+
+# number of value columns corresponding to each plot name
+# --> R-G-B-I
+num_val_col_per_plot <- 4
+
+# calculate the total number of value columns
+total_val_col <- num_plot_names * num_val_col_per_plot
+
+# get the unique plot names from the 'kspnr' column in bi_plots_cropped_buf
+# (repeated for each set of value columns)
+plot_names <- rep(unique(bi_plots_cropped_buf$kspnr), 
+                  each = num_val_col_per_plot)
+
+# create vector of indices for the value columns to keep
+val_col_indices <- c(seq(2,5), seq(7,10), seq(12,15), seq(17,20), 
+                     seq(22,25), seq(27,30), seq(32,35), seq(37,40),
+                     seq(42,45), seq(47,50))
+
+# subset the data frame to keep only the value columns
+extracted_val_dop <- extracted_val_dop[, val_col_indices]
+
+# assign the plot names as column names
+colnames(extracted_val_dop) <- plot_names
+
+# create band names:
+# -R = red, -G = green, -B = blue, -I = near-infrared
+bands <- c('-R', '-G', '-B', '-I')
+
+# create new column names with band names
+# and assign them to extracted_val_dop
+new_col_names <- paste0(colnames(extracted_val_dop), bands)
+colnames(extracted_val_dop) <- new_col_names
+head(extracted_val_dop)
 
 
 
@@ -158,6 +222,7 @@ head(extracted_val_df)
 
 # --> potential explanatory variables for a timber volume model
 #
+# nDSM:
 # height metrics: mean, standard deviation, minimum, maximum,
 # percentile values (1st, 5th, 10th, 20th, 25th, 30th, 40th, 50th,
 #                    60th, 70th, 75th, 80th, 90th, 95th, 99th);
@@ -165,8 +230,14 @@ head(extracted_val_df)
 # (all three as conventional moments and as L-moments),
 # canopy relief ratio (crr) --> https://doi.org/10.1016/j.foreco.2003.09.001;
 # canopy cover metrics: percentage of pixels above 3m and above mean height
+#
+# DOP:
+# spectral metrics: NDVI
+#
 # --> see different studies, e.g. https://doi.org/10.1139/cjfr-2014-0297
-plot_metrics <- sapply(extracted_val_df, function(z)
+#
+# metrics based on the nDSM
+plot_metrics <- sapply(extracted_val_ndsm, function(z)
   c(mean = mean(z, na.rm = T),
     sd   = sd(z, na.rm = T),
     min  = min(z, na.rm = T),
@@ -186,13 +257,45 @@ plot_metrics <- sapply(extracted_val_df, function(z)
     pabove3 = (sum(z > 3, na.rm = T) / length(z)) * 100,
     pabovemean = (sum(z > mean(z, na.rm = T), na.rm = T) / length(z)) * 100))
 
+# metrics based on the DOP
+
+# define NDVI function
+calculate_ndvi <- function(red, nir) {
+  
+  (nir - red) / (nir + red)
+  
+}
+
+# calculate mean NDVI for each plot
+ndvi <- vector('numeric', length = ncol(extracted_val_dop) / 4)
+
+for (i in seq(1, ncol(extracted_val_dop), by = 4)) {
+  
+  # get red and nir values for each plot
+  red <- extracted_val_dop[, i]
+  nir <- extracted_val_dop[, i + 3]
+  
+  # apply NDVI function to each plot
+  plot_ndvi <- calculate_ndvi(red, nir)
+  ndvi[(i + 3) / 4] <- mean(plot_ndvi, na.rm = T)
+}
+
+# create data frame with NDVI values and plot names
+ndvi_plots <- data.frame(plot = unique(bi_plots_cropped_buf$kspnr),
+                         NDVI = ndvi)
+
+# add NDVI values for each plot to plot_metrics data frame
+plot_metrics <- rbind(plot_metrics, ndvi)
+
+#----------------------------------------------------------
+
 # transpose the result for a more convenient format
 plot_metrics_transposed <- t(plot_metrics)
 
 # add volumes per sample plot
 plot_metrics_transposed <- cbind(plot_metrics_transposed, bi_plots_cropped_buf$vol_ha)
 plot_metrics_transposed <- as.data.frame(plot_metrics_transposed)
-names(plot_metrics_transposed)[29] <- 'vol_ha'
+names(plot_metrics_transposed)[30] <- 'vol_ha'
 head(plot_metrics_transposed)
 
 
