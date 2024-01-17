5626c8a8f0a7789e1d7e83f3cba2df8a8a70a85d
FloFranz <florian.franz86@gmx.de>
2024-01-17 09:03:41 +0100

    expand point cloud extraction to two forestry offices

--- scripts/pc_ctg_extraction.R
+++ scripts/pc_ctg_extraction.R
@@ -1,7 +1,7 @@
 #--------------------------------------------------------------------------
 # Name:         pc_ctg_extraction.R
 # Description:  Script extracts multiple polygons of point clouds from a
-#               point cloud catalog located in a desired forestry office.
+#               point cloud catalog located in desired forestry offices.
 #               The extracted point clouds are written to disk.
 # Author:       Florian Franz
 # Contact:      florian.franz@nw-fva.de
@@ -37,7 +37,7 @@ lidR::plot(ndsm_pc_ctg)
 
 # read BI data preprocessed in script vol_sample_plots.R
 # contains timber volume per sample points
-bi_plots <- sf::st_read(paste0(processed_data_dir, 'vol_stp_Kopie_GR_092023.gpkg'))
+bi_plots <- sf::st_read(paste0(processed_data_dir, 'vol_stp_092023.gpkg'))
 
 # quick overview
 bi_plots
@@ -53,39 +53,39 @@ str(nlf_org)
 # 03 - data preparation
 #-------------------------------------
 
-# filter plots by year (2022) and forestry office (Neuhaus = 268)
-bi_plots_neuhaus <- bi_plots[grep('268-2022-', bi_plots$key),]
+# filter plots by year (2022) and forestry offices (Neuhaus = 268, Dassel = 254)
+bi_plots_solling <- bi_plots[grep('268-2022|254-2022', bi_plots$key),]
 
 # filter administrative forestry data
-# by forestry office 'Neuhaus' (268) 
-fa_neuhaus <- nlf_org[nlf_org$FORSTAMT == 268,]
+# by forestry offices 'Neuhaus' (268) and 'Dassel' (254) 
+fa_solling <- nlf_org[nlf_org$FORSTAMT == 268 | nlf_org$FORSTAMT == 254,]
 
 # assign CRS to point clouds (ETRS89 / UTM zone 32N)
 lidR::crs(ndsm_pc_ctg) <- 'EPSG:25832'
 
 # reproject BI plots to the CRS of the point clouds
 # DHDN / 3-degree Gauss-Kruger zone 3 --> ETRS89 / UTM zone 32N
-bi_plots_neuhaus <- sf::st_transform(bi_plots_neuhaus, sf::st_crs(25832))
+bi_plots_solling <- sf::st_transform(bi_plots_solling, sf::st_crs(25832))
 
 # visualize locations of BI plots
 lidR::plot(ndsm_pc_ctg)
-terra::plot(bi_plots_neuhaus$geom, col = 'red', add = T)
-terra::plot(fa_neuhaus$geometry, alpha = 0, lwd = 2, add = T)
+terra::plot(bi_plots_solling$geom, col = 'red', add = T)
+terra::plot(fa_solling$geometry, alpha = 0, lwd = 2, add = T)
 
 # get polygons (squares) of point clouds in the catalog
-# that are in the area of the desired forestry office
-ctg_polys_neuhaus <- sf::st_crop(ndsm_pc_ctg@data[["geometry"]],
-                                 fa_neuhaus)
+# that are within the area of the desired forestry offices
+ctg_polys_solling <- sf::st_intersection(ndsm_pc_ctg@data[["geometry"]],
+                                         sf::st_union(fa_solling))
 
-ctg_polys_neuhaus <- sf::st_sf(ctg_polys_neuhaus)
+ctg_polys_solling <- sf::st_sf(ctg_polys_solling)
 
 # quick overview
-terra::plot(ctg_polys_neuhaus)
-terra::plot(bi_plots_neuhaus$geom, add= T, col = 'red')
-terra::plot(fa_neuhaus$geometry, alpha = 0, lwd = 2, add = T)
+terra::plot(ctg_polys_solling)
+terra::plot(bi_plots_solling$geom, add= T, col = 'red')
+terra::plot(fa_solling$geometry, alpha = 0, lwd = 2, add = T)
 
 # extract the point clouds that are in the
-# area of the desired forestry office
-dir.create(paste0(processed_data_dir, 'nDSMs_laz_neuhaus'))
-lidR::opt_output_files(ndsm_pc_ctg) <- paste0(processed_data_dir, 'nDSMs_laz_neuhaus/{XLEFT}_{YBOTTOM}_{ID}')
-ndsm_pc_ctg_neuhaus <- lidR::clip_roi(ndsm_pc_ctg, ctg_polys_neuhaus)
+# area of the desired forestry offices
+dir.create(paste0(processed_data_dir, 'nDSMs_laz_solling'))
+lidR::opt_output_files(ndsm_pc_ctg) <- paste0(processed_data_dir, 'nDSMs_laz_solling/{XLEFT}_{YBOTTOM}_{ID}')
+ndsm_pc_ctg_solling <- lidR::clip_roi(ndsm_pc_ctg, ctg_polys_solling)
